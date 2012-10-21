260c1fe6b96b680dc38430f4d9e95476987d82ba
Misa Sakamoto <msakamoto@nascentdigital.com>
2012-10-20 22:51:41 -0400

    setup pages complete :)

--- cookright/Shared/Classes/Controllers/CRSetupViewController.m
+++ cookright/Shared/Classes/Controllers/CRSetupViewController.m
@@ -8,8 +8,20 @@
 #pragma mark - Class Extension
 
 @interface CRSetupViewController ()
+{
+    @private __weak UIButton *_button;
+    @private __strong UIImageView *_page1;
+    @private __strong UIImageView *_page2;
+    @private __strong UIImageView *_page3;
+    @private __strong UIImageView *_page4;
+}
+
+@property (nonatomic, weak) IBOutlet UIButton *button;
 
 - (void)_initializeSetupViewController;
+- (IBAction)showPage2: (id)sender;
+- (IBAction)showPage3: (id)sender;
+- (IBAction)showPage4: (id)sender;
 - (IBAction)showMainRootController: (id)sender;
 
 
@@ -90,14 +102,6 @@
 }
 
 
-#pragma mark - Destructor
-
-- (void)dealloc 
-{
-	// nil out delegates of any instance variables.
-}
-
-
 #pragma mark - Public Methods
 
 
@@ -109,6 +113,28 @@
 	[super viewDidLoad];
 	
 	// Perform additional initialization after nib outlets are bound.
+    
+    CGRect frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+    CGRect frameHidden = CGRectMake(frame.size.width, 0, frame.size.width, frame.size.height);
+    _page1 = [[UIImageView alloc]
+        initWithImage: [UIImage imageNamed: @"Welcome_1.png"]];
+    _page1.frame = frame;
+    _page2 = [[UIImageView alloc]
+        initWithImage: [UIImage imageNamed: @"Welcome_2_Foods.png"]];
+    _page2.frame = frameHidden;
+    _page3 = [[UIImageView alloc]
+        initWithImage: [UIImage imageNamed: @"Welcome_3_Foods_Edited.png"]];
+    _page3.frame = frameHidden;
+    _page4 = [[UIImageView alloc]
+        initWithImage: [UIImage imageNamed: @"Welcome_4_Cook_Number.png"]];
+    _page4.frame = frameHidden;
+    
+    [self.view addSubview: _page1];
+    [self.view addSubview: _page2];
+    [self.view addSubview: _page3];
+    [self.view addSubview: _page4];
+    
+    [self.view bringSubviewToFront: self.button];
 }
 
 - (void)viewWillAppear: (BOOL)animated
@@ -157,6 +183,48 @@
 - (void)_initializeSetupViewController
 {
 	// Initialize instance variables.
+}
+
+- (IBAction)showPage2: (id)sender
+{
+    [UIView beginAnimations: @"showPage2"
+        context: nil];
+    [UIView setAnimationDuration: 0.5f];
+    _page2.frame = _page1.frame;
+    [UIView commitAnimations];
+    
+    // Set up the next button action.
+    [self.button addTarget: self
+        action: @selector(showPage3:)
+        forControlEvents: UIControlEventTouchUpInside];
+}
+
+- (IBAction)showPage3: (id)sender
+{
+    [UIView beginAnimations: @"showPage3"
+        context: nil];
+    [UIView setAnimationDuration: 0.5f];
+    _page3.frame = _page1.frame;
+    [UIView commitAnimations];
+    
+    // Set up the next button action.
+    [self.button addTarget: self
+        action: @selector(showPage4:)
+        forControlEvents: UIControlEventTouchUpInside];
+}
+
+- (IBAction)showPage4: (id)sender
+{
+    [UIView beginAnimations: @"showPage4"
+        context: nil];
+    [UIView setAnimationDuration: 0.5f];
+    _page4.frame = _page1.frame;
+    [UIView commitAnimations];
+    
+    // Set up the next button action.
+    [self.button addTarget: self
+        action: @selector(showMainRootController:)
+        forControlEvents: UIControlEventTouchUpInside];
 }
 
 - (IBAction)showMainRootController: (id)sender
